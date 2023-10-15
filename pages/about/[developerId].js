e35f87dcd435f1c723cccb96ba18c8553fddf430
divyagurav <132669878+divyagurav@users.noreply.github.com>
2023-10-15 21:11:17 +0530

    Add files via upload

--- pages/about/[developerId].js
+++ pages/about/[developerId].js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+
 const DeveloperPage = () => {
   const router = useRouter();
 
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,27 @@
 import React from "react";
+import Link from "next/link";
 const Aboutus = () => {
-  return <h1>The AboutUs</h1>;
+  const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
+  return (
+    <>
+      <h1>Name of our teams </h1>
+
+      {details.map((data) => (
+        <div>
+          <Link href={`about/${data.id}`}>Name:{data.name}</Link>
+        </div>
+      ))}
+      {/* {details.map((data) => (
+        <div>
+          <Link href={`about/${data.id}`}>Name : {data.name}</Link>
+        </div>
+      ))} */}
+    </>
+  );
 };
 
 export default Aboutus;
